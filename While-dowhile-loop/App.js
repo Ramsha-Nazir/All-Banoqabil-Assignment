eddb910ba358941852955db3bf67d6cc19f70c6d
Ramsha-Nazir <sahabsardar216@gmail.com>
2025-06-12 20:34:58 -0700

    Add files via upload

---
+++ While-dowhile-loop/App.js
@@ -0,0 +1,13 @@
+// while
+let count = 0;
+while (count < 5) {
+    count++;
+    console.log("Hello, World!", count);
+}
+
+// Do-while
+let i = 1;
+do {
+  console.log("Do while:", i);
+  i++;
+} while (i <= 5);
