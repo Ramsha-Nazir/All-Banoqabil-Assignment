fa5e8604b43a55392d8973660875fec4d74de42e
Ramsha-Nazir <sahabsardar216@gmail.com>
2025-06-12 20:16:52 -0700

    Add files via upload

---
+++ Math/App.js
@@ -0,0 +1,9 @@
+let num = 5.9;
+
+console.log("I entered:", num);
+console.log("Rounded:", Math.round(num));
+console.log("Square Root:", Math.sqrt(num));
+console.log("Ceil:", Math.ceil(num));
+console.log("Floor:", Math.floor(num));
+console.log("Random Number:", Math.random());
+console.log("Power of 2:",Math.pow(num,2));
